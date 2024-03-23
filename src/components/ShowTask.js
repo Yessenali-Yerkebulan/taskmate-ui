db8978915506aa012ec74c4df8c68093f994f07c
Yerkebulan <yerkebulanyessenali03@gmail.com>
2024-03-23 15:08:20 +0500

    add functionality to add task and count all tasks

--- src/components/ShowTask.js
+++ src/components/ShowTask.js
@@ -1,30 +1,27 @@
-import React from 'react'
+export const ShowTask = ({tasklist, setTasklist}) => {
 
-export const ShowTask = () => {
-    const tasks = [
-    ];
-
-  return (
-    <section className="showTask">
-        <div className="head">
-            <div>
-                <span className="title">Todo</span>
-                <span className="count">0</span>
-            </div>
-            <button className="clearAll">Clear All</button>
-        </div>
-        <ul>
-            {tasks.map((task) => (
-                 <li>
-                 <p>
-                     <span className="name">{task.name}</span>
-                     <span className="time">{task.time}</span>
-                 </p>
-                 <i className="bi bi-pencil-square"></i>
-                 <i className="bi bi-trash"></i>
-             </li>
-            ))}
-        </ul>
-    </section>
-  )
-}
+    return (
+      <section className="showTask">
+          <div className="head">
+              <div>
+                  <span className="title">Todo</span>
+                  <span className="count">{tasklist.length}</span>
+              </div>
+              <button className="clearAll" onClick={() => setTasklist([])}>Clear All</button>
+          </div>
+          <ul>
+              { tasklist.map((task) => (
+                  <li key={task.id}>
+                      <p>
+                          <span className="name">{task.name}</span>
+                          <span className="time">{task.time}</span>
+                      </p>
+                      <i className="bi bi-pencil-square"></i>
+                      <i className="bi bi-trash"></i>
+                  </li>
+              )) }            
+          </ul>
+      </section>
+    )
+  }
+  
